53029446ee1216ac532590e67713e38563e77ee1
Ioanna M Dimitriou H <ioanna.m.dimitriou@gmail.com>
2018-02-06 16:07:58 +0100

    fixed fluent-bit invocation and instructions

--- start/start-fluentbit.sh
+++ start/start-fluentbit.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-echo "Starting fluentbit with elasticsearch plugin." 
-echo "See documentation here:"
-echo 'http://fluentbit.io/documentation/current/output/elasticsearch.html'
+echo "Starting fluentbit"
 
 if [ "${FLUENTBIT_CONFIG_FILE}" == "" ]
 then echo "No config file given, starting Fluent-bit with the test config: in_dummy.conf"
-     echo "To start Fluent-bit with another config file, say FOO.conf, place it in a directory, for example in /BAR and start the container with the option:"
-     echo "-v BAR:/BAR -e FLUENTBIT_CONFIG_FILE=BAR/FOO.conf"
-     FLUENTBIT_CONFIG_FILE="in_dummy.conf"
-else echo "Starting Fluent-bit with configuration file ${FLUENTBIT_CONFIG_FILE}, in container dir ${FLUENTBIT_CONFIG_DIR}."
+     echo "To start Fluent-bit with another config file, say FOO.conf, place it in a directory, for example in ./BAR/ and start the container with the option:"
+     echo "-v /full/path/to/BAR/FOO.conf:${FLUENTBIT_HOME}/FOO.conf -e FLUENTBIT_CONFIG_FILE=${FLUENTBIT_HOME}/FOO.conf"
+     FLUENTBIT_CONFIG_FILE="${FLUENTBIT_CONFIG_DIR}/in_dummy.conf"
+else echo "Starting Fluent-bit with configuration file ${FLUENTBIT_CONFIG_FILE}"
 fi
 
-${FLUENTBIT_HOME}/fluent-bit -c ${FLUENTBIT_CONFIG_DIR}/${FLUENTBIT_CONFIG_FILE}
+${FLUENTBIT_HOME}/fluent-bit -c ${FLUENTBIT_CONFIG_FILE}
 
